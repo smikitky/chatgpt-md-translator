464d09b74aa3bd2eb190f6fdd977fafbbb1d1ff1
Soichiro Miki <smiki-tky@umin.ac.jp>
2024-04-03 18:59:24 +0900

    Add more friendly error messages to readTextFile

--- src/fs-utils.ts
+++ src/fs-utils.ts
@@ -2,15 +2,19 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 
 // We use this to output a bit frindlier error
-
 export const readTextFile = async (filePath: string): Promise<string> => {
   try {
     return await fs.readFile(filePath, 'utf-8');
   } catch (e: any) {
-    if (e.code === 'ENOENT') {
-      throw new Error('File not found: ' + filePath);
-    } else {
-      throw e;
+    switch (e.code) {
+      case 'EISDIR':
+        throw new Error('The specified path is a directory: ' + filePath);
+      case 'ENOENT':
+        throw new Error('File not found: ' + filePath);
+      case 'EACCES':
+        throw new Error('Permission denied: ' + filePath);
+      default:
+        throw e;
     }
   }
 };
